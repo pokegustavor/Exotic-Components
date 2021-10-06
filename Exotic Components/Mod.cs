e0bb3f7b8250df9d6fef3c7fd95ef88e0f5e1efa
Gustavo Ribeiro <pokegustavo@gmail.com>
2021-10-05 23:37:30 -0300

    1.1

--- Exotic Components/Mod.cs
+++ Exotic Components/Mod.cs
@@ -4,7 +4,7 @@ namespace Exotic_Components
 {
     public class Mod : PulsarMod
     {
-        public override string Version => "1.0";
+        public override string Version => "1.1";
 
         public override string Author => "pokegustavo";
 
